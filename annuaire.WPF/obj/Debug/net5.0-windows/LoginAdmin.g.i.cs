a48605ae8aeb529e9b5f049ff45918f12acbe9b6
Kemrode <kevinfichou@gmail.com>
2022-03-07 22:26:17 +0100

    (presentation): add annuaireµ.WPF

--- annuaire.WPF/obj/Debug/net5.0-windows/LoginAdmin.g.i.cs
+++ annuaire.WPF/obj/Debug/net5.0-windows/LoginAdmin.g.i.cs
@@ -85,7 +85,7 @@ namespace annuaire.WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/annuaire.WPF;component/loginadmin.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/annuaire.WPF;V1.0.0.0;component/loginadmin.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\LoginAdmin.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
